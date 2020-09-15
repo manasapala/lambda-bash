1125b922f57febd9b602cdce30af9f1f54538efc
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-09-15 10:53:23 -0700

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,29 +1,13 @@
-#!/bin/bash
-# core.sh
-# simple shell script to be executed by aws lambda nodejs wrapper
-# Author: @JacobBaloul
+#!/bin/bash	
+	ARTIFACT=`packer build -machine-readable gold-ami.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+	AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
+	echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
+	echo $AMI_ID
+	terraform init
+	terraform apply
+	aws ssm put-parameter \
+	--name "AMI_ID" \
+	--value $AMI_ID \
+	--type String \
+	--data-type "aws:ec2:image"
 
-
-###
-### MAIN
-###
-	echo "=START="
-	
-	# TODO: 
-	#	Customize below...
-	# 	any bash commands and shell-fu here.
-	#	you can package your existing shell scripts and call them here
-	#
-	echo "BASH RANDOM: $RANDOM" && seq 1 18 && echo {19..36}
-	uname -a
-	uptime
-	cat /proc/cpuinfo
-	free -m
-	df -h
-	date
-	ls -ltrh / /root /home /var /var/log
-	cat /etc/passwd
-	whoami
-	
-	echo "=END="
-###
